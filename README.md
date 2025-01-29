24afd31b219d6c0b4b24213e2e73d7c0f65c5317
Bibhu Prasad Samal <bibhusprasad@gmail.com>
2025-01-29 21:02:25 +0530

    code refactored move to different packages

--- README.md
+++ README.md
@@ -10,7 +10,7 @@
 * [Initial Commit](https://github.com/bibhusprasad/019_graphql_with_springboot/commit/7becc2e8b75c849a38a32a84639bfa4187b13753)
 * [First Hello World API call](https://github.com/bibhusprasad/019_graphql_with_springboot/commit/52099057b7742d5ecb99c482295fce8c3cb35d48)
 * [Query passing argument](https://github.com/bibhusprasad/019_graphql_with_springboot/commit/2abfcff9acdc1c035fdf61469fdbc6a809800a34)
-* [Query passing argument 2]()
+* [Query passing argument 2](https://github.com/bibhusprasad/019_graphql_with_springboot/commit/604b2c5636f18d9d8dc8fe6ec13520a7c14525ec)
 
 
 
--- src/main/java/com/bibhu/graphql/sec01/helloworld/HelloWorldController.java
+++ src/main/java/com/bibhu/graphql/sec01/helloworld/HelloWorldController.java
@@ -1,8 +1,10 @@
-package com.bibhu.graphql.helloworld;
+package com.bibhu.graphql.sec01.helloworld;
 
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 @Controller
 public class HelloWorldController {
@@ -25,5 +27,10 @@ public class HelloWorldController {
     @QueryMapping("sayHelloToAny")
     public String sayHelloToAnyName(@Argument("name") String value) {
         return "Say Hello World To - " + value;
+    }
+
+    @QueryMapping("randomNo")
+    public int generateRandomNumber() {
+        return ThreadLocalRandom.current().nextInt(1, 100);
     }
 }
